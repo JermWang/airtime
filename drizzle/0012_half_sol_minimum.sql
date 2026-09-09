1607d7e89591f2fa741a76011dc2025a2a53304a
JermWang <meesdontmiss@gmail.com>
2026-09-08 22:45:08 -0500

    Normalize migration and devnet test file formatting

--- drizzle/0012_half_sol_minimum.sql
+++ drizzle/0012_half_sol_minimum.sql
@@ -2,4 +2,3 @@
 UPDATE placements SET auction = jsonb_set(jsonb_set(auction, '{openingPriceWei}', to_jsonb(GREATEST((auction->>'openingPriceWei')::numeric, 500000000)::text)), '{floorPriceWei}', to_jsonb(GREATEST((auction->>'floorPriceWei')::numeric, 500000000)::text));
 --> statement-breakpoint
 UPDATE showcase_creatives SET sublabel = replace(sublabel, '0.01 SOL', '0.5 SOL') WHERE sublabel LIKE '%0.01 SOL%';
-
